2f63cb5452972a33650dd30ee445f2a191bb7d0a
amari1990 <amari@ilerna.com>
2024-11-11 12:56:12 +0100

    change adios to holi

--- src/main/example.java
+++ src/main/example.java
@@ -5,7 +5,7 @@ public class example {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola");
-		System.out.println("adios");
+		System.out.println("holi");
 	}
 
 }
